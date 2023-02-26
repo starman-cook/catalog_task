78e3d1ad202a0f87187379201bff8a5e8c44f040
Pasha Mishakov <pashamishakov@gmail.com>
2023-02-27 04:35:20 +0600

    add api and slice to books

--- frontend_part/src/api/booksApi.ts
+++ frontend_part/src/api/booksApi.ts
@@ -2,12 +2,14 @@ import IBook from "../interfaces/IBook"
 import IResponse from "../interfaces/IResponse"
 import { AxiosResponse } from 'axios'
 import { instance } from "./instance"
+import IBookDto from "../interfaces/IBookDto"
+import IBookAuthor from "../interfaces/IBookAuthor"
 
 
 class BooksApi {
     public getBooks = async (title?: string): Promise<IResponse> => {
         try {
-            const books: AxiosResponse<IBook[]> = await instance.get('/authors', title ? {params: {query: {title}}} : undefined)
+            const books: AxiosResponse<IBook[]> = await instance.get('/books', title ? {params: {query: {title}}} : undefined)
             const response: IResponse = {
                 message: 'Success',
                 result: books.data
@@ -22,6 +24,79 @@ class BooksApi {
             return response
         }
     }
+
+    public getBookById = async (id: number): Promise<IResponse> => {
+        try {
+            const books: AxiosResponse<IBook> = await instance.get(`/books/${id}`)
+            const response: IResponse = {
+                message: 'Success',
+                result: books.data
+            }
+            return response
+        } catch (err: unknown) {
+            const error = err as Error
+            const response: IResponse = {
+                message: error.message,
+                result: null
+            }
+            return response
+        }
+    }
+
+    public deleteBook = async (id: number): Promise<IResponse> => {
+        try {
+            const books: AxiosResponse<number> = await instance.delete(`/books/${id}`)
+            const response: IResponse = {
+                message: 'Success',
+                result: books.data
+            }
+            return response
+        } catch (err: unknown) {
+            const error = err as Error
+            const response: IResponse = {
+                message: error.message,
+                result: null
+            }
+            return response
+        }
+    }
+
+    public createBook = async (book: IBookDto): Promise<IResponse> => {
+        try {
+            const books: AxiosResponse<IBook> = await instance.post('/books', book)
+            const response: IResponse = {
+                message: 'Success',
+                result: books.data
+            }
+            return response
+        } catch (err: unknown) {
+            const error = err as Error
+            const response: IResponse = {
+                message: error.message,
+                result: null
+            }
+            return response
+        }
+    }
+
+    public addAuthor = async (bookAuthor: IBookAuthor): Promise<IResponse> => {
+        try {
+            const books: AxiosResponse<IBookAuthor> = await instance.post('/books/add-author', bookAuthor)
+            const response: IResponse = {
+                message: 'Success',
+                result: books.data
+            }
+            return response
+        } catch (err: unknown) {
+            const error = err as Error
+            const response: IResponse = {
+                message: error.message,
+                result: null
+            }
+            return response
+        }
+    }
+
 }
 
 export const booksApi = new BooksApi()
--- frontend_part/src/store/books/booksSlice.ts
+++ frontend_part/src/store/books/booksSlice.ts
@@ -1,6 +1,8 @@
 import { createSlice } from '@reduxjs/toolkit'
 import { booksApi } from '../../api/booksApi'
 import IBook from '../../interfaces/IBook'
+import IBookAuthor from '../../interfaces/IBookAuthor'
+import IBookDto from '../../interfaces/IBookDto'
 import { createAppAsyncThunk } from '../createAppAsyncThunk'
 import IBooksState from './IBooksState'
 
@@ -10,6 +12,34 @@ export const getBooks = createAppAsyncThunk(
     `${namespace}/getBooks`,
     async (title?: string) => {
         return await booksApi.getBooks(title)
+    }
+)
+
+export const getBookById = createAppAsyncThunk(
+    `${namespace}/getBookById`,
+    async (id: number) => {
+        return await booksApi.getBookById(id)
+    }
+)
+
+export const deleteBook = createAppAsyncThunk(
+    `${namespace}/deleteBook`,
+    async (id: number) => {
+        return await booksApi.deleteBook(id)
+    }
+)
+
+export const createBook = createAppAsyncThunk(
+    `${namespace}/createBook`,
+    async (book: IBookDto) => {
+        return await booksApi.createBook(book)
+    }
+)
+
+export const addAuthor = createAppAsyncThunk(
+    `${namespace}/addAuthor`,
+    async (bookAuthor: IBookAuthor) => {
+        return await booksApi.addAuthor(bookAuthor)
     }
 )
 
@@ -34,6 +64,49 @@ export const booksSlice = createSlice({
             .addCase(getBooks.fulfilled, (state, action) => {
                 state.loadingBooks = false
                 state.books = action.payload.result as IBook[]
+                state.messageBooks = action.payload.message
+            })
+            .addCase(getBookById.rejected, (state) => {
+                state.loadingBooks = false
+            })
+            .addCase(getBookById.pending, (state) => {
+                state.loadingBooks = true
+            })
+            .addCase(getBookById.fulfilled, (state, action) => {
+                state.loadingBooks = false
+                state.book = action.payload.result as IBook | null
+                state.messageBooks = action.payload.message
+            })
+            .addCase(deleteBook.rejected, (state) => {
+                state.loadingBooks = false
+            })
+            .addCase(deleteBook.pending, (state) => {
+                state.loadingBooks = true
+            })
+            .addCase(deleteBook.fulfilled, (state, action) => {
+                state.loadingBooks = false
+                state.deleteBookResult = action.payload.result as number
+                state.messageBooks = action.payload.message
+            })
+            .addCase(createBook.rejected, (state) => {
+                state.loadingBooks = false
+            })
+            .addCase(createBook.pending, (state) => {
+                state.loadingBooks = true
+            })
+            .addCase(createBook.fulfilled, (state, action) => {
+                state.loadingBooks = false
+                state.book = action.payload.result as IBook
+                state.messageBooks = action.payload.message
+            })
+            .addCase(addAuthor.rejected, (state) => {
+                state.loadingBooks = false
+            })
+            .addCase(addAuthor.pending, (state) => {
+                state.loadingBooks = true
+            })
+            .addCase(addAuthor.fulfilled, (state, action) => {
+                state.loadingBooks = false
                 state.messageBooks = action.payload.message
             })
     }
